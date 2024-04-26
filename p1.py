1698d74535be24af91aec6f03217f5b6f5ad0378
Prajwal2711 <prajwalhedau27@gmail.com>
2024-04-26 20:37:13 +0530

    Add new feature

---
+++ p1.py
@@ -0,0 +1,45 @@
+# x = 3
+# y = str(x)
+# print(type(x))
+
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(fruits[0])
+
+# import random
+# print(random.randrange(1, 10))
+
+# a = '''1aaaaa
+# 2aaaaaa'''
+# print(a)
+
+# age = 36
+# txt = "My name is John, and I am {}"
+# print(txt.format(age))
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(2, "watermelon")
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# print(thislist)
+# thislist = ["apple", "banana", "cherry"]
+# del thislist
+
+# a = 2
+# b = 330
+# print("A") if a > b else print("B")
+
+# def myFun(arg1, arg2, arg3):
+# 	print("arg1:", arg1)
+# 	print("arg2:", arg2)
+# 	print("arg3:", arg3)
+# args = ("Geeks", "for", "Geeks")
+# myFun(*args)
+# kwargs = {"arg1": "Geeks", "arg2": "for", "arg3": "Geeks"}
+# myFun(**kwargs)
+
+# x = lambda a, b : a * b
+# print(x(5, 6))
